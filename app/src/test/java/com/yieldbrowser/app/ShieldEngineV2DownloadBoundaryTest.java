d342dbd7e8e0287f21175f4d9b5f59d6fc5f3992
yoskidays <yoskimaster1@gmail.com>
2026-07-18 14:39:55 +0700

    Stage 101: tighten Ratudrakor download boundary tests

--- app/src/test/java/com/yieldbrowser/app/ShieldEngineV2DownloadBoundaryTest.java
+++ app/src/test/java/com/yieldbrowser/app/ShieldEngineV2DownloadBoundaryTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 
 public class ShieldEngineV2DownloadBoundaryTest {
     private static final String DOWNLOAD_PAGE =
-            "https://ratudrakor.biz.id/2026/07/08/download-drama-korea-see-you-at-work-tomorrow-episode-4-subtitle-indonesia/";
+            "https://ratudrakor.biz.id/2026/07/16/download-drama-korea-see-you-at-work-tomorrow-episode-8-subtitle-indonesia/";
 
     @Test
     public void downloadArticleIsNotMisclassifiedAsReaderPage() {
@@ -17,15 +17,30 @@ public class ShieldEngineV2DownloadBoundaryTest {
     }
 
     @Test
-    public void allowsUserInitiatedCleanDownloadDestinations() {
-        String mirror = "https://mirror.example.org/download/see-you-at-work-episode-4";
-        String directFile = "https://cdn.example.org/files/see-you-at-work-episode-4-720p.mkv";
+    public void allowsUserInitiatedTrustedDownloadDestinations() {
+        String mediaFire = "https://www.mediafire.com/file/abc123/see-you-at-work-episode-8.mkv/file";
+        String googleDrive = "https://drive.google.com/file/d/abc123456789/view";
+        String directFile = "https://cdn.example.org/files/see-you-at-work-episode-8-720p.mkv";
 
-        assertTrue(ShieldEngineV2.isSafeDownloadNavigation(mirror, DOWNLOAD_PAGE, true));
+        assertTrue(ShieldEngineV2.isSafeDownloadNavigation(mediaFire, DOWNLOAD_PAGE, true));
+        assertTrue(ShieldEngineV2.isSafeDownloadNavigation(googleDrive, DOWNLOAD_PAGE, true));
         assertFalse(ShieldEngineV2.shouldBlockMainFrameNavigation(
-                mirror, DOWNLOAD_PAGE, true, true, false, false));
+                mediaFire, DOWNLOAD_PAGE, true, true, false, false));
+        assertFalse(ShieldEngineV2.shouldBlockMainFrameNavigation(
+                googleDrive, DOWNLOAD_PAGE, true, true, false, false));
         assertFalse(ShieldEngineV2.shouldBlockMainFrameNavigation(
                 directFile, DOWNLOAD_PAGE, true, true, false, false));
+    }
+
+    @Test
+    public void genericDownloadLookingLandingPageCannotMasqueradeAsFileHost() {
+        String disguisedAdLanding =
+                "https://unknown-download.example/download/see-you-at-work-episode-8";
+
+        assertFalse(ShieldEngineV2.isSafeDownloadNavigation(
+                disguisedAdLanding, DOWNLOAD_PAGE, true));
+        assertTrue(ShieldEngineV2.shouldBlockMainFrameNavigation(
+                disguisedAdLanding, DOWNLOAD_PAGE, true, true, false, false));
     }
 
     @Test
@@ -43,10 +58,10 @@ public class ShieldEngineV2DownloadBoundaryTest {
 
     @Test
     public void crossSiteDownloadRequiresARealUserGesture() {
-        String mirror = "https://mirror.example.org/download/see-you-at-work-episode-4";
+        String mediaFire = "https://www.mediafire.com/file/abc123/episode-8.mkv/file";
 
-        assertFalse(ShieldEngineV2.isSafeDownloadNavigation(mirror, DOWNLOAD_PAGE, false));
+        assertFalse(ShieldEngineV2.isSafeDownloadNavigation(mediaFire, DOWNLOAD_PAGE, false));
         assertTrue(ShieldEngineV2.shouldBlockMainFrameNavigation(
-                mirror, DOWNLOAD_PAGE, false, true, false, false));
+                mediaFire, DOWNLOAD_PAGE, false, true, false, false));
     }
 }
